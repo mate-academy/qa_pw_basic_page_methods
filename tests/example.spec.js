d0c6a3c6bd024dc279743a4dd76e9d76cb6d8262
Inna Ianko <inna.i@mate.academy>
2024-12-20 11:47:10 +0200

    Add missing test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,3 +5,9 @@ test('Sign In link test', async ({ page }) => {
   await page.getByRole('link', { name: 'Sign in' }).click();
   await page.waitForURL('https://conduit.mate.academy/user/login');
  });
+
+ test('Sign UP link test', async ({ page }) => {
+  // Add your code below this line:
+ 
+ });
+ 
